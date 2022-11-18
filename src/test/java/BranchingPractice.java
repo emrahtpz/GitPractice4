5a99b835fe79af95c24d218db00ea9d971cb6087
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-18 23:29:07 +0300

    Beneficial comments added

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,8 @@ public class BranchingPractice {
     public static void main(String[] args) {
         System.out.println("Commited");
         System.out.println("Another day another dollar");
+
+        // we can keep ignoring some files we forget to ignore, such as mispelling mistakes
+        // we can do git rm --caches -r .idea
     }
 }
