3bca2e7de1962b2765a5b07d1b28b8cfb1bfac00
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-18 23:04:12 +0300

    Trying to fix

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,6 @@
 public class BranchingPractice {
     public static void main(String[] args) {
         System.out.println("Commited");
+        System.out.println("Another day another dollar");
     }
 }
